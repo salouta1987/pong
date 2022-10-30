9ca59b4e1160f98e19da4d0c12a971cd8728ff97
salouta1987 <112333794+salouta1987@users.noreply.github.com>
2022-10-30 15:21:35 +0100

    Initial commit

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,21 @@
+
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello Word!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hello Update!");
+
+
+
+    }          
+    
+}
